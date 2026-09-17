10b5ee9dc6b18fd95b9331b20084740e6526c880
leodube <contact@leodube.ca>
2026-09-17 00:40:32 +0000

    fix(webapp): disable pino formatting in production

--- webapp/server/src/tools/logger.ts
+++ webapp/server/src/tools/logger.ts
@@ -1,20 +1,26 @@
 import pino from "pino";
 import { env } from "../config/env";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export const logger = pino({
   level: env.logging.level,
-  transport: {
-    target: "pino-http-print",
-    options: {
-      all: true,
-      colorize: true,
-      translateTime: "SYS:HH:MM:ss",
+  ...(isProduction
+    ? {}
+    : {
+        transport: {
+          target: "pino-http-print",
+          options: {
+            all: true,
+            colorize: true,
+            translateTime: "SYS:HH:MM:ss",
 
-      // Optional: Configure how pino-pretty formats your internal/decorator logs
-      prettyOptions: {
-        translateTime: "SYS:HH:MM:ss",
-        ignore: "pid,hostname"
-      }
-    }
-  }
+            // Optional: Configure how pino-pretty formats your internal/decorator logs
+            prettyOptions: {
+              translateTime: "SYS:HH:MM:ss",
+              ignore: "pid,hostname"
+            }
+          }
+        }
+      })
 });
